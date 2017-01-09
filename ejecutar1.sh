cc3965c066672d1cc35ea8e8fe38a69d6fe351f0
Arafo <rafa_marcen@hotmail.com>
2017-01-09 12:07:17 +0100

    Más pruebas

--- ejecutar1.sh
+++ ejecutar1.sh
@@ -39,12 +39,12 @@ java -cp bin practica1.Main -p 200 -t 0 -r 10 -a ks -n 3 -u 0 -d 0 -fm grafos/re
 #java -cp bin practica1.Main -p 500 -t 0 -r 10 -a ks -n 3 -u 0 -d 0 -fm grafos/relaciones500b.dat -fp grafos/productos500b.dat
 
 # 10 repeticiones del algoritmo de Karger con el generador Random y con una matriz 10x10 de enteros.
-java -cp bin practica1.Main -p 10 -t 1 -r 10 -a ks -n 0 -u 1 -d 0 -fm grafos/relaciones10e.dat -fp grafos/productos10e.dat
+java -cp bin practica1.Main -p 10 -t 1 -r 10 -a k -n 0 -u 1 -d 0 -fm grafos/relaciones10e.dat -fp grafos/productos10e.dat
 # 10 repeticiones del algoritmo de Karger con el generador SecureRandom y con una matriz 50x50 de enteros.
-java -cp bin practica1.Main -p 50 -t 1 -r 10 -a ks -n 1 -u 1 -d 0 -fm grafos/relaciones50e.dat -fp grafos/productos50e.dat
+java -cp bin practica1.Main -p 50 -t 1 -r 10 -a k -n 1 -u 1 -d 0 -fm grafos/relaciones50e.dat -fp grafos/productos50e.dat
 # 10 repeticiones del algoritmo de Karger con el generador HighQualityRandom y con una matriz 100x100 de enteros.
-java -cp bin practica1.Main -p 100 -t 1 -r 10 -a ks -n 2 -u 1 -d 0 -fm grafos/relaciones100e.dat -fp grafos/productos100e.dat
+java -cp bin practica1.Main -p 100 -t 1 -r 10 -a k -n 2 -u 1 -d 0 -fm grafos/relaciones100e.dat -fp grafos/productos100e.dat
 # 10 repeticiones del algoritmo de Karger con el generador XORShiftRandom y con una matriz 200x200 de enteros.
-java -cp bin practica1.Main -p 200 -t 1 -r 10 -a ks -n 3 -u 1 -d 0 -fm grafos/relaciones200e.dat -fp grafos/productos200e.dat
+java -cp bin practica1.Main -p 200 -t 1 -r 10 -a k -n 3 -u 1 -d 0 -fm grafos/relaciones200e.dat -fp grafos/productos200e.dat
 # 10 repeticiones del algoritmo de Karger con el generador XORShiftRandom y con una matriz 500x500 de enteros.
 #java -cp bin practica1.Main -p 500 -t 1 -r 10 -a ks -n 3 -u 1 -d 0 -fm grafos/relaciones500e.dat -fp grafos/productos500e.dat
