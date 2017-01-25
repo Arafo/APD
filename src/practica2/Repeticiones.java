fcf1438cc265dfdefafb18baecd248ace9036fe4
josan <josan@192.168.1.128>
2017-01-25 16:52:35 +0100

    Añadidas funciones para encontrar la repeticion mas larga y las repeticiones maximales (+ de 2 letras)

--- src/practica2/Repeticiones.java
+++ src/practica2/Repeticiones.java
@@ -7,5 +7,11 @@ public class Repeticiones {
 	public static void main(String[] args) {
 		ArbolSufijosCompacto arbol = new ArbolSufijosCompacto("bananas");
 		System.out.println(arbol.raiz);
+		System.out.println("Repeticion mas larga");
+		System.out.println(arbol.repeticionMasLarga());
+		System.out.println("Repeticion maximales:");
+		for(String s:arbol.repeticionesMaximales()){
+			System.out.println("	->"+s);
+		}
 	}
 }
--- src/practica2/arbolsufijos/ArbolSufijosCompacto.java
+++ src/practica2/arbolsufijos/ArbolSufijosCompacto.java
@@ -1,6 +1,7 @@
 package practica2.arbolsufijos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArbolSufijosCompacto extends ArbolSufijos {
@@ -61,4 +62,63 @@ public class ArbolSufijosCompacto extends ArbolSufijos {
 		}
 		return nodo;		
 	}
+	
+	
+	/**
+	 * Encuentra la repeticion mas larga dentro del arbol de sufijos
+	 * @return
+	 */
+	public String repeticionMasLarga() {
+		int N = super.texto.length();
+		String[] sufijos = new String[N];
+		for (int i = 0; i < N; i++) {
+			sufijos[i] = super.texto.substring(i, N);
+		}
+		// sort them
+		Arrays.sort(sufijos);
+		// find longest repeated substring by comparing adjacent sorted suffixes
+		String lrs = "";
+		for (int i = 0; i < N - 1; i++) {
+			String x = repeticionMasLarga(sufijos[i], sufijos[i + 1]);
+			if (x.length() > lrs.length())
+				lrs = x;
+		}
+		return lrs;
+	}
+	
+	private  String repeticionMasLarga(String s, String t){
+		int n = Math.min(s.length(), t.length());
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != t.charAt(i))
+                return s.substring(0, i);
+        }
+        return s.substring(0, n);		
+	}
+	
+	
+	public ArrayList<String> repeticionesMaximales(){
+		ArrayList<String> maximales=new ArrayList<String>();
+		int N = super.texto.length();
+		String[] sufijos = new String[N];
+		for (int i = 0; i < N; i++) {
+			sufijos[i] = super.texto.substring(i, N);
+		}
+		// sort them
+		Arrays.sort(sufijos);
+		// find maximal > 2
+		for (int i = 0; i < N - 1; i++) {
+			String x = repeticionMasLarga(sufijos[i], sufijos[i + 1]);
+			if (x.length() >= 2)
+				maximales.add(x);
+		}
+		return maximales;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
