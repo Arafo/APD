761dadc6942cb858548696d6fe534adcd1f8ce98
Arafo <rafa_marcen@hotmail.com>
2017-01-26 19:22:39 +0100

    Añadidas mediciones de tiempos

--- src/practica2/Repeticiones.java
+++ src/practica2/Repeticiones.java
@@ -7,45 +7,85 @@ import java.util.Map;
 import practica2.arbolsufijos.ArbolSufijosCompacto;
 
 public class Repeticiones {
+		
+	public static boolean debug = true;
+	public static String FICHERO_GENES = "genes/12S.fasta";
 
 	public static void main(String[] args) {
 		
-		/*LectorGenes lg = new LectorGenes("genes/12S.fasta");
+		System.out.println("Fichero: " + FICHERO_GENES);
+		System.out.println("--------------------------------");
+		LectorGenes lg = new LectorGenes(FICHERO_GENES);
 		Hashtable<String, String> genes = lg.obtenerGenes();
+		
+		long[] tiemposCreacion = new long[genes.size()];
+		long[] tiemposRepeticion = new long[genes.size()];
+		long[] tiemposMaximal = new long[genes.size()];
+		int indice = 0;
 		
 		Iterator<Map.Entry<String, String>> it = genes.entrySet().iterator();
 		while (it.hasNext()) { 
 			Map.Entry<String, String> entry = it.next();
-			System.out.println(entry.getKey());			
-			System.out.println(entry.getValue());
-		}*/
+			/* Arbol compacto */
+			if (debug)
+				System.out.print("Creando arbol compacto "+ entry.getKey() + "...");
+			
+			long tiempo = System.currentTimeMillis();
+			ArbolSufijosCompacto arbol = new ArbolSufijosCompacto(entry.getValue());
+			tiempo = System.currentTimeMillis() - tiempo;
+			tiemposCreacion[indice] = tiempo;
 
-		String cadena_ADN = "AATAGGTTTGGTCCTAGCCTTTCTATTAGCTCTTAGTAAGATTACACATGCAAGCATCCC"
-				+ "CGTTCCAGTGAGTTCACCCTCTAAATCACCACGATCAAAAGGGACAAGCATCAAGCACGC"
-				+ "AGCAATGCAGCTCAAAACGCTTAGCCTAGCCACACCCCCACGGGAAACAGCAGTGATTAA"
-				+ "CCTTTAGCAATAAACGAAAGTTTAACTAAGCTATACTAACCCCAGGGTTGGTCAATTTCG"
-				+ "TGCCAGCCACCGCGGTCACACGATTAACCCAAGTCAATAGAAGCCGGCGTAAAGAGTGTT"
-				+ "TTAGATCACCCCCTCCCCAATAAAGCTAAAACTCACCTGAGTTGTAAAAAACTCCAGTTG"
-				+ "ACACAAAATAGACTACGAAAGTGGCTTTAACATATCTGAACACACAATAGCTAAGACCCA"
-				+ "AACTGGGATTAGATACCCCACTATGCTTAGCCCTAAACCTCAACAGTTAAATCAACAAAA"
-				+ "CTGCTCGCCAGAACACTACGAGCCACAGCTTAAAACTCAAAGGACCTGGCGGTGCTTCAT"
-				+ "ATCCCTCTAGAGGAGCCTGTTCTGTAATCGATAAACCCCGATCAACCTCACCACCTCTTG"
-				+ "CTCAGCCTATATACCGCCATCTTCAGCAAACCCTGATGAAGGCTACAAAGTAAGCGCAAG"
-				+ "TACCCACGTAAAGACGTTAGGTCAAGGTGTAGCCCATGAGGTGGCAAGAAATGGGCTACA"
-				+ "TTTTCTACCCCAGAAAACTACGATAGCCCTTATGAAACTTAAGGGTCGAAGGTGGATTTA"
-				+ "GCAGTAAACTGAGAGTAGAGTGCTTAGTTGAACAGGGCCCTGAAGCGCGTACACACCGCC"
-				+ "CGTCACCCTCCTCAAGTATACTTCAAAGGACATTTAACTAAAACCCCTACGCATTTATAT"
-				+ "AGAGGAGACAAGTCGTAACATGGTAAGTGTACTGGAAAGTGCACTTGGACGAAC";
+			if (debug) {
+				System.out.println(" HECHO");
+				//System.out.println(arbol.raiz);
+				System.out.println("Tiempo: " + tiempo + " ms");
+				System.out.println("--------------------------------");
+			}
 
-		ArbolSufijosCompacto arbol = new ArbolSufijosCompacto(cadena_ADN);
-		System.out.println(arbol.raiz);
-		System.exit(0);
-		
-		System.out.println("Repeticion mas larga");
-		System.out.println(arbol.repeticionMasLarga());
-		System.out.println("Repeticion maximales:");
-		for (String s : arbol.repeticionesMaximales()) {
-			System.out.println("	->" + s);
+			/* Repeticion mas larga */
+			if (debug)
+				System.out.print("Repeticion mas larga: ");
+			
+			tiempo = System.currentTimeMillis();
+			System.out.println(arbol.repeticionMasLarga());
+			tiempo = System.currentTimeMillis() - tiempo;
+			tiemposRepeticion[indice] = tiempo;
+			
+			if (debug) {
+				System.out.println("Tiempo: " + tiempo + " ms");
+			}
+			
+			/* Repeticiones maximales*/
+			if (debug) {
+				System.out.println("--------------------------------");
+				System.out.println("Repeticion maximales:");
+			}
+			
+			tiempo = System.currentTimeMillis();
+			for (String s : arbol.repeticionesMaximales()) {
+				System.out.println("	->" + s);
+			}
+			tiempo = System.currentTimeMillis() - tiempo;
+			tiemposMaximal[indice] = tiempo;
+
+			if (debug) {
+				System.out.println("Tiempo: " + tiempo + " ms");
+				System.out.println("--------------------------------");
+			}		
+			
+			indice++;
 		}
+		
+		/* Tiempos medios */
+		long tiempoCreacionMedio = 0, tiempoRepeticionMedio = 0, tiempoMaximalMedio = 0;
+		for (int i = 0; i < genes.size(); i++) {
+			tiempoCreacionMedio += tiemposCreacion[i];
+			tiempoRepeticionMedio += tiemposRepeticion[i];
+			tiempoMaximalMedio += tiemposMaximal[i];
+		}
+		System.out.println("TIEMPO MEDIO CREACION ARBOL: " + tiempoCreacionMedio / genes.size() + " ms");
+		System.out.println("TIEMPO MEDIO REPETICION MAS LARGA: " + tiempoRepeticionMedio / genes.size() + " ms");
+		System.out.println("TIEMPO MEDIO REPETICION MAXIMAL: " + tiempoMaximalMedio / genes.size() + " ms");
+
 	}
 }
